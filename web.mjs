4fb0e617446368828e4b30bcf32037d471fd91b5
이은지 <dev.nogglee@gmail.com>
2025-05-25 17:25:54 +0900

    test

--- web.mjs
+++ web.mjs
@@ -497,20 +497,20 @@ class ModalComponent extends HTMLElement {
 		document.body.style.overflow = 'hidden';
 		modal.querySelector('#modal_title').textContent = item.title;
 		modal.querySelector('#modal_description').innerHTML = item.description;
-		const link = modal.querySelector('#modal_link');
-		link.style.display = 'none';
+		// const link = modal.querySelector('#modal_link');
+		// link.style.display = 'none';
 
-		if (originType === 'template') {
-			link.setAttribute('href', item.link);
-			link.textContent = '템플릿 바로가기';
-			link.style.display = 'block';
-		} else if (originType === 'portfolio') {
-			if (item.link && item.link.trim() !== '') {
-				link.setAttribute('href', item.link);
-				link.textContent = '서비스 바로가기';
-				link.style.display = 'block';
-			}
-		}
+		// if (originType === 'template') {
+		// 	link.setAttribute('href', item.link);
+		// 	link.textContent = '템플릿 바로가기';
+		// 	link.style.display = 'block';
+		// } else if (originType === 'portfolio') {
+		// 	if (item.link && item.link.trim() !== '') {
+		// 		link.setAttribute('href', item.link);
+		// 		link.textContent = '서비스 바로가기';
+		// 		link.style.display = 'block';
+		// 	}
+		// }
 		modal.querySelector('#modal_content').innerHTML = item.content ?? '';
 
 		const iframe = modal.querySelector('#modal_video');
